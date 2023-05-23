b5fb00486c4fc60d8657cb23caefbb9f15a53112
divya <divyajoshics09@gmail.com>
2023-05-23 10:54:52 +0300

    updating farebase config and commit

---
+++ src/config/firebase.Config.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyB1o1pX9wS6lHHASDkEc6DG-AcrAOTv81s",
+  authDomain: "cryptoworld-1f0cf.firebaseapp.com",
+  projectId: "cryptoworld-1f0cf",
+  storageBucket: "cryptoworld-1f0cf.appspot.com",
+  messagingSenderId: "931486147172",
+  appId: "1:931486147172:web:8304e99e5b08cd6e55b571"
+};
+  export default firebaseConfig;
